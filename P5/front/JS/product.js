817d05ca9625aff3d70a54f2ee77bb231af95f53
Nawk94 <r.alexandre94@gmail.com>
2023-01-22 17:00:02 +0100

    modified:   P5/front/JS/product.js 	modified:   .DS_Store 	modified:   P5/front/JS/cart.js 	modified:   P5/front/JS/script.js

--- P5/front/JS/product.js
+++ P5/front/JS/product.js
@@ -6,7 +6,7 @@ const ident = factor.get("_id");
 
 //récupération de l'id dans le fetch
 fetch("http://localhost:3000/api/products" + ident)
-    .then((reponse) => response.json())
+    .then((response) => response.json())
     .then((products) => showProducts(products))
     .catch((error) => alert("Chargement impossible, le serveur ne répond pas"));
 
@@ -17,6 +17,8 @@ function showProducts(product) {
 	let picProduct = document.createElement("img");
 	picProduct.src = product.imageUrl;
 	picProduct.alt = product.altTxt;
+	//parent + enfant
+	picElement.appendChild(picProduct);
 	let descrElement = document.querySelector("#description");
 	descrElement.innerText = product.description;
 	let titlElement = document.querySelector("#title");
@@ -25,12 +27,12 @@ function showProducts(product) {
 	costElement.innerText = product.price;
 
 
-	//parent + enfant
-	picElement.appendChild(picProduct);
+
 
 
 	//choix des différentes couleurs 
 	let choiceColor = document.getElementById("colors");
+
 	product.colors.foreach(selectColor => {
 		let difColor = document.createElement("option");
 		difColor.value = `${selectColor}`;
@@ -41,15 +43,76 @@ function showProducts(product) {
 
 	});
 
-	//ajout des articles au panier
+	//ajout des articles au panier en écoutant le bouton et empêchement d'actualisation de la page 
 
 	const listenElement = document.getElementById("addToCart");
-	listenElement.addEventListener("click"), (event) => {
+	listenElement.addEventListener("click", (event) => {
 		event.preventDefault();
 
 		//récupération de la selection
 		const choiceColor = document.getElementById("colors");
 		const quantityElement = document.querySelector("#quantity");
-	};
 
+		//items à enregistrer 
+		let choice = {
+			id : ident,
+			color : `${choiceColor.value}`,
+			quantity : `${quantityElement.value}`,
+		};
+
+		//ajout de  la selection dans le localstorage
+
+		//transformation en tableau
+		let itemsLocalStorage = JSON.parse(localStorage.getItem("product")); 
+
+		let basketUpdate = false;
+
+		const additemsLocalStorage = () => {
+			itemsLocalStorage.push(choice);
+			//transformation en chaîne de caractères 
+			localStorage.setItem("product", JSON.stringify(itemsLocalStorage));
+		};
+
+		let confirmText = () => {
+			alert ('Le canapé a été ajouté à votre panier');
+		};
+
+		// vérifie que 1 <= canapé <= 100 et qu'une couleur est sélectionée
+		function quantityAndColorChoice () {
+			if (choice.quantity < 1 ||
+				choice.quantity > 100 ||
+				choice.quantity === undefined ||
+				choice.color === "" ||
+				choice.color === undefined
+			) {
+				alert ("Veuillez choisir une couleur pour votre canapé avant de l'ajouter à votre panier")
+			} else if
+				//si le local storage contient déja des produits avec la même ident et la même couleur et si des produits sont présent avec les même key
+				(itemsLocalStorage) {
+				itemsLocalStorage.forEach(function (product, key){
+					if (product.id == ident && product.color == choiceColor.value){
+						itemsLocalStorage[key].quantity = parseInt(product.quantity) + parseInt(quantityElement.value);
+						localStorage.setItem("product", JSON.stringify(itemsLocalStorage));
+						basketUpdate = true;
+						confirmText();
+					}
+				});
+
+				if (!basketUpdate) {
+					additemsLocalStorage();
+					confirmText;
+				}
+
+			}
+			// finalement, si aucun produit n'existe dans le local storage
+			else {
+				itemsLocalStorage = [];
+				additemsLocalStorage();
+				confirmText();
+			}
+		};
+
+		quantityAndColorChoice();
+
+	});
 };
